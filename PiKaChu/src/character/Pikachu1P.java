dcbbaa8a290d8d97dc8279f240a8e6ee84738097
qscesz84563 <qscesz84563@gmail.com>
2020-05-31 13:36:38 +0800

    Update Pikachu1P.java

--- PiKaChu/src/character/Pikachu1P.java
+++ PiKaChu/src/character/Pikachu1P.java
@@ -5,7 +5,6 @@ import java.awt.Graphics;
 import java.util.Dictionary;
 import java.util.Hashtable;
 
-import volleyball.Game;
 import volleyball.Game2PMode;
 import javax.swing.*;
 
@@ -27,59 +26,54 @@ public class Pikachu1P extends Player{
 		feature_num.put("hitCount", 0);
 		feature_num.put("moveSpeedX", 5);
 		feature_num.put("moveSpeedY", 5);
+		feature_num.put("score", 0);
 		
 		feature_bool.put("jumping", false);
 		feature_bool.put("goingUp", false);
 		feature_bool.put("goingDown", false);
 	}
+
 	public void update() {
-		// up_1P
-		if(game.getKeyManager().up_1P & !feature_bool.get("jumping")) {
+		//up_1P
+		if(game.getKeyManger().up_1P & !feature_bool.get("jumping")) {
 			feature_bool.put("jumping", true);
 			feature_bool.put("goingUp", true);
 			feature_bool.put("goingDown", false);
 		}
-		
-		// left_1P
-		if(game.getKeyManager().left_1P) {
+
+		//left_1P
+		if(game.getKeyManger().left_1P) {
 			feature_num.put("moveSpeedX", -7);
 			feature_num.put("x", feature_num.get("x") + feature_num.get("moveSpeedX"));
 			
 			game.player1.setLocation(feature_num.get("x"), feature_num.get("y"));
 		}
-		
+
 		// right_1P
-		if(game.getKeyManager().right_1P){
+		if(game.getKeyManger().right_1P){
 			feature_num.put("moveSpeedX", 7);
 			feature_num.put("x", feature_num.get("x") + feature_num.get("moveSpeedX"));
 			
 			game.player1.setLocation(feature_num.get("x"), feature_num.get("y"));
 		}
-		
+
 		// jumping
 		if(feature_bool.get("jumping")) {
-			// going up 
 			if(feature_bool.get("goingUp")){
 				feature_num.put("moveSpeedY", -15);
-				
 				if(feature_num.get("y") <= 200) {
 					feature_bool.put("goingUp", false);
 					feature_bool.put("goingDown", true);
 				}
-				
 			} else if(feature_bool.get("goingDown")){
-				// going_down
 				feature_num.put("moveSpeedY", 15);
 				feature_bool.put("goingDown", false);
 			}
 			
-			// move
 			feature_num.put("y", feature_num.get("y") + feature_num.get("moveSpeedY"));
 			feature_num.put("moveSpeedY", feature_num.get("moveSpeedY") + 1);
-			
 			game.player1.setLocation(feature_num.get("x"), feature_num.get("y"));
 			
-			// set y
 			if(feature_num.get("y") + feature_num.get("height") >= game.height) {
 				feature_bool.put("jumping", false);
 				feature_bool.put("goingDown", false);
@@ -121,24 +115,42 @@ public class Pikachu1P extends Player{
 		this.feature_num.put("hitCount", hitCount);
 	}
 	
+	public void setScore(int score) {
+		this.feature_num.put("score", score);
+	}
+	
+	public void setLocation(int x, int y) {
+		this.feature_num.put("x", x);
+		this.feature_num.put("y", y);
+		game.player1.setLocation(x, y);
+	}
+	
 	public int getHitCount() {
 		return feature_num.get("hitCount");
 	}
+
 
 	public int getX() {
 		return feature_num.get("x");
 	}
 
+
 	public int getY() {
 		return feature_num.get("y");
 	}
+
 
 	public int getWidth() {
 		return feature_num.get("width");
 	}
 
+
 	public int getHeight() {
 		return feature_num.get("height");
+	}
+	
+	public int getScore() {
+		return feature_num.get("score");
 	}
 
 }
